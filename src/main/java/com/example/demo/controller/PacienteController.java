d7087affaa805d18d0438fe358865fecdb49e592
Alyne Matos <alynemmatos@gmail.com>
2023-07-28 12:22:15 -0300

    feat: add metodo get, update e delete

--- src/main/java/com/example/demo/controller/PacienteController.java
+++ src/main/java/com/example/demo/controller/PacienteController.java
@@ -24,5 +24,35 @@ public class PacienteController {
         BeanUtils.copyProperties(pacienteRecordDTO, paciente);
         return ResponseEntity.status(HttpStatus.CREATED).body(pacienteRepository.save(paciente));
     }
+    @GetMapping("/paciente")
+    public ResponseEntity<List<Paciente>> getAllPaciente(){
+        return ResponseEntity.status(HttpStatus.OK).body(pacienteRepository.findAll());
+    }
+    @GetMapping("/paciente/{id}")
+    public ResponseEntity<Object> getPacienteById(@PathVariable Long id){
+        Optional<Paciente> pacienteID = pacienteRepository.findById(id);
+        if(pacienteID.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("not found");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(pacienteID.get());
+    }
+
+    @PutMapping("/paciente/{id}")
+    public ResponseEntity<Paciente> putPacienteById(@PathVariable(value="id") Long id, @RequestBody @Valid PacienteRecordDTO pacienteRecordDTO ){
+        Optional<Paciente> pacienteUP = pacienteRepository.findById(id);
+        var paciente = pacienteUP.get();
+        BeanUtils.copyProperties(pacienteRecordDTO,paciente);
+        return ResponseEntity.status(HttpStatus.OK).body(pacienteRepository.save(paciente));
+    }
+
+    @DeleteMapping("/paciente/{id}")
+    public ResponseEntity<Object> deletePaciente(@PathVariable(value="id") Long id) {
+        Optional<Paciente> pacienteDel = pacienteRepository.findById(id);
+        if(pacienteDel.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Paciente not found.");
+        }
+        pacienteRepository.delete(pacienteDel.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Paciente deleted successfully.");
+    }
 
 }
